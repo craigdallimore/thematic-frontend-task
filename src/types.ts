45db62f3f32a691baecff8d0bf6396fd605b09c2
craigdallimore <decoy9697@gmail.com>
2022-05-10 12:40:44 +1200

    feat: add types related to filter form feature

--- src/types.ts
+++ src/types.ts
@@ -12,12 +12,61 @@ export type Synopsis = {
   numRows: number;
 };
 
+export type SynopsisError = {
+  code: string;
+  description: string;
+};
+
 export type SynopsisResponse =
   | {
       data: Synopsis;
       status: "success";
     }
+  | SynopsisError;
+
+export type FilterType = "Default" | "Data" | "Search" | "Score";
+export type ScoreType = "Average" | "NPS" | "Threshold";
+
+export type FilterId = string;
+
+export type Filter = {
+  id: FilterId;
+  name: string;
+  type: FilterType;
+  scoreType: null | ScoreType;
+};
+
+export type FilterState = {
+  synopsis: null | Synopsis;
+  error: null | SynopsisError;
+  filters: Filter[];
+};
+
+export type FilterAction =
   | {
-      code: string;
-      description: string;
+      type: "FILTER_ADDED";
+      payload: Filter;
+    }
+  | {
+      type: "FILTER_UPDATED";
+      payload: Filter;
+    }
+  | {
+      type: "FILTER_DELETED";
+      payload: FilterId;
+    }
+  | {
+      type: "FILTERS_UPDATED";
+      payload: Filter[];
+    }
+  | {
+      type: "FILTERS_CLEARED";
+    }
+  | {
+      type: "SYNOPSIS_RECEIVED";
+      payload: Synopsis;
+    }
+  | {
+      type: "SYNOPSIS_ERROR_RECEIVED";
+      payload: SynopsisError;
     };
